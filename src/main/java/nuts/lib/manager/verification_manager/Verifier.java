6c32752b16bb1c813358edb81b8caff17788605b
MIMminE <weluv.ww@gmail.com>
2024-06-10 17:29:31 +0900

    빌더 클래스 검증용 로직 수정 (필수 필드 이름 표시되도록)

--- src/main/java/nuts/lib/manager/verification_manager/Verifier.java
+++ src/main/java/nuts/lib/manager/verification_manager/Verifier.java
@@ -2,6 +2,7 @@ package nuts.lib.manager.verification_manager;
 
 import java.util.function.Supplier;
 
+@Deprecated(since = "2024. 06. 10")
 public interface Verifier<T> {
 
     boolean condition();
